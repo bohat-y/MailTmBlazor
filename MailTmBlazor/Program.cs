db4d7946dfbcbcfaba0bd06ce7d4a245b33b283a
bohat-y <bohatyrr@gmail.com>
2025-10-21 23:42:19 +0100

    mailtm client class

--- MailTmBlazor/Program.cs
+++ MailTmBlazor/Program.cs
@@ -1,3 +1,4 @@
+using DefaultNamespace;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MailTmBlazor;
@@ -6,6 +7,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://api.mail.tm/") // TODO: refactor this
+    
+});
+
+builder.Services.AddScoped<MailTmClient>();
+
 
 await builder.Build().RunAsync();
