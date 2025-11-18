4c1d3741ecba726810b8a0e913cc0d8e3e4b3215
bohat-y <bohatyrr@gmail.com>
2025-11-18 15:35:34 +0000

    playwright + unit tests baby steps

---
+++ tests/playwright-js/playwright.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, devices } from '@playwright/test';
+import 'dotenv/config';
+
+const baseURL = process.env.PLAYWRIGHT_BASE_URL || 'https://mailtmblazor-abghg7bggth2fkbg.switzerlandnorth-01.azurewebsites.net';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    baseURL,
+    trace: 'on-first-retry'
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    }
+  ]
+});
